d566e1b500d86feee0e72e2a927953c30b5c2bce
Josh <josh@josh-macbook-air.local>
2016-02-01 11:45:14 -0800

    Add script to install Docker toolbox

---
+++ script/install_dockertoolbox.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+_setup_toolbox() {
+    if ! which "brew" > /dev/null; then
+        echo_info "Please install brew first!\n"
+        exit 1
+    fi
+
+    # Unlink old brew-cask
+    if which "brew-cask" > /dev/null; then
+        echo_info "Unlinking old brew-cask...\n"
+        echo
+        brew unlink brew-cask
+        echo
+    fi
+
+    # Install cask
+    if [ ! -d /opt/homebrew-cask ]; then
+        echo "Brew cask doesn't look like it is installed"
+        echo -n "Do you want to install it? [y]"
+        read n1 ans
+        echo
+        ans=$(echo $ans | tr Y y)
+        if [ -n "$ans" ] && [ "$ans" != "y" ]; then
+            echo "Aborting."
+            exit 1
+        else
+            brew tap caskroom/cask
+        fi
+    fi
+
+    echo
+    echo_info "Updating brew...\n"
+    echo
+    brew update
+    echo
+
+    echo_info "Adding brew cask for Docker Toolbox...\n"
+    echo
+    brew install caskroom/cask/brew-cask
+    brew link brew-cask
+    echo
+
+    echo_info "Installing Docker Toolbox...\n"
+    echo
+    brew cask install dockertoolbox
+    echo
+}
+
+_setup_toolbox
