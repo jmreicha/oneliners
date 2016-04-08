47bb52dbe4615a02ea6e888263c00b5945d29fe6
Josh Reichardt <josh.reichardt@gmail.com>
2016-04-08 10:58:24 -0700

    Make installer smarter

--- script/install_dockertoolbox.sh
+++ script/install_dockertoolbox.sh
@@ -7,50 +7,114 @@ echo_info () {
 }
 
 _setup_toolbox() {
+    # Install brew
     if ! which "brew" > /dev/null; then
-        echo_info "Please install brew first!\n"
-        exit 1
+        echo "Homebrew doesn't look like it is installed"
+        read -rn1 -p "Do you want to install it? [y] " ans
+        echo
+        local ans
+        ans=$(echo "$ans" | tr Y y)
+        if [ -n "$ans" ] && [ "$ans" != "y" ]; then
+            echo "Aborting."
+            exit 1
+        else
+            xcode-select --install || true
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
     fi
 
     # Unlink old brew-cask
     if which "brew-cask" > /dev/null; then
-        echo_info "Unlinking old brew-cask...\n"
+        echo_info "Unlinking old brew-cask..."
         echo
         brew unlink brew-cask
         echo
     fi
 
-    # Install cask
-    if [ ! -d /opt/homebrew-cask ]; then
-        echo "Brew cask doesn't look like it is installed"
-        echo -n "Do you want to install it? [y]"
-        read n1 ans
+    echo
+    echo_info "Updating brew..."
+    echo
+
+    brew update
+
+    if ! which node > /dev/null; then
         echo
-        ans=$(echo $ans | tr Y y)
-        if [ -n "$ans" ] && [ "$ans" != "y" ]; then
-            echo "Aborting."
-            exit 1
-        else
-            brew tap caskroom/cask
-        fi
+        echo_info "Installing node..."
+        brew install node
     fi
 
     echo
-    echo_info "Updating brew...\n"
-    echo
-    brew update
+    echo_info "Updating brew cask..."
     echo
 
-    echo_info "Adding brew cask for Docker Toolbox...\n"
-    echo
-    brew install caskroom/cask/brew-cask
-    brew link brew-cask
-    echo
+    # This doesn't actually do anything except activate `brew cask` if it
+    # hasn't already been tapped. Otherwise `brew update` also does these
+    # updates.
+    brew cask update
 
-    echo_info "Installing Docker Toolbox...\n"
+    # Check the installed docker version vs. the available version on berw cask
+    local docker_version
+    local dockertoolbox_version
+    # Get the current version of the docker command and strip trailing commas
+    docker_version=$(docker --version 2>/dev/null | awk '{print $3}' | sed 's/,$//' || true)
+    # Get the current version of the docker toolbox cask and strip trailing letters
+    dockertoolbox_version=$(brew cask info dockertoolbox | head -1 | awk '{print $2}' | sed 's/[a-z]*$//')
+
+    # If we don't have matching versions, either it's not installed or it's out
+    # of date, so we'll install or upgrade (it's the same)
+    if [[ ! "$docker_version" = "$dockertoolbox_version" ]]; then
+        echo
+        if [ -z "$docker_version" ]; then
+            echo_info "Installing Docker Toolbox..."
+        else
+            echo_info "Updating Docker Toolbox..."
+            echo_info "Stopping running VMs..."
+            echo
+            _stop_vms
+        fi
+        echo
+
+        # Attempt to get the currenet VirtualBox version
+        local vbox_version
+        vbox_version=$(vboxmanage -v 2>/dev/null || true)
+
+        # Do the install/upgrade
+        brew cask install dockertoolbox
+
+        # Check if we need to upgrade the VBox guest additions
+        if [[ "$vbox_version" != "$(vboxmanage -v)" ]]; then
+            _install_vbox_additions
+        fi
+
+        if [ -n "$docker_version" ]; then
+            echo
+            echo_info "Starting VMs..."
+            echo
+            _start_vms
+        fi
+    else
+        echo
+        echo_info "Docker Toolbox up to date, skipping..."
+    fi
+}
+
+_install_vbox_additions() {
     echo
-    brew cask install dockertoolbox
+    echo_info "Installing VBox Guest Additions..."
     echo
+    full_version=$(vboxmanage -v)
+    version=$(echo "$full_version" | cut -d 'r' -f 1)
+    revision=$(echo "$full_version" | cut -d 'r' -f 2)
+    file="Oracle_VM_VirtualBox_Extension_Pack-$version-$revision.vbox-extpack"
+
+    # Download the extension pack
+    curl -sL http://download.virtualbox.org/virtualbox/"$version"/"$file" > /tmp/"$file"
+
+    # Install the Guest Additions
+    sudo VBoxManage extpack install /tmp/"$file" --replace
+
+    # Remove downloaded files
+    rm /tmp/"$file"
 }
 
 _setup_toolbox
